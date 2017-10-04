684a68c6b6bd89e7d2e81dbdcf26397c9eacfdd0
mikesorae <lovepoko247@gmail.com>
2017-10-04 20:11:10 +0900

    allow Entity inheritance

--- src/main/kotlin/net/straycalico/ecsample/domain/common/Entity.kt
+++ src/main/kotlin/net/straycalico/ecsample/domain/common/Entity.kt
@@ -3,7 +3,7 @@ package net.straycalico.ecsample.domain.common
 /**
  * Entityを表すクラス
  */
-class Entity<T: Any, ID : Identifier<T>> (val id: Identifier<T>) {
+open class Entity<T: Any, ID : Identifier<T>> (val id: Identifier<T>) {
     fun equals(other: Entity<T, Identifier<T>>): Boolean {
         return when (other) {
             is Entity<T, Identifier<T>> -> this.id == other.id
--- src/test/kotlin/net/straycalico/ecsample/domain/common/EntityTest.kt
+++ src/test/kotlin/net/straycalico/ecsample/domain/common/EntityTest.kt
@@ -16,4 +16,15 @@ class EntityTest {
         val id = Identifier(1)
         assert(Entity(id).equals(Entity(id)))
     }
+
+    @Test
+    fun testInherit() {
+        class MyId(value: String): Identifier<String>(value)
+        class MyEntity(id: MyId): Entity<String, MyId>(id)
+
+        val id = MyId("id-001")
+        val entity = MyEntity(id)
+
+        assertEquals("id-001", entity.id.value)
+    }
 }
