12e9809f667eee18795e6cf87fc75738c2e66574
mikesorae <lovepoko247@gmail.com>
2017-10-05 17:28:03 +0900

    define search method for order

--- src/main/kotlin/net/straycalico/ecsample/domain/order/OrderRepository.kt
+++ src/main/kotlin/net/straycalico/ecsample/domain/order/OrderRepository.kt
@@ -4,6 +4,6 @@ import net.straycalico.ecsample.domain.customer.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import javax.transaction.Transactional
 
-@Transactional(Transactional.TxType.MANDATORY)
 interface OrderRepository : JpaRepository<Order, Long> {
+    fun findOneByOrderId(orderId: OrderId): Order
 }
